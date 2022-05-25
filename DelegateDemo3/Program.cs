0b6201c4cc8421535e77c1588a819b56ed4e5fc8
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 10:21:33 +0530

    first commit

---
+++ DelegateDemo3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo3
+{
+    public delegate int Calculation(int x, int y);
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Calculation calc = (x, y) => x + y;
+            int result = calc(25, 30);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
